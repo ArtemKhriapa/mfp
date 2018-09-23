75dcba9164596a2911e38eece9db64f5299d830d
Alex Shevelo <oshevelo@softserveinc.com>
2018-09-23 12:47:43 +0300

    fix settings file

--- mfp/manage.py
+++ mfp/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mfp.local_settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mfp.setting")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
