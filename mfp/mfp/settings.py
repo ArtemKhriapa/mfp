da5418e1396b9d2335bae72d1969c5e509d39457
Alex Shevelo <oshevelo@softserveinc.com>
2018-09-23 12:54:11 +0300

    auth app added + local config

--- mfp/mfp/settings.py
+++ mfp/mfp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+#    'apps.auth',
     # 'social_django',
 
     # 'apps.otc',
@@ -162,3 +162,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+from local import *
