9c236ddaf7c8ba82ba0b85aa8b5b2e7e25ae7fb2
levashovn <levashovn322@gmail.com>
2018-10-21 12:41:23 +0300

    conflict

--- mfp/mfp/settings.py
+++ mfp/mfp/settings.py
@@ -37,12 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    #'apps.mfp-auth',
     'social_django',
-
     'apps.otc',
     'rest_framework',
-    'cards',
+    'cards'
 ]
 
 MIDDLEWARE = [
