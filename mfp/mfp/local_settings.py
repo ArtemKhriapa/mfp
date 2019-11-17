790cabb6643857d47a3dd88e37b7220fc0ad21f3
lyklike <39054327+Yanlebed@users.noreply.github.com>
2019-11-17 20:53:12 +0200

    Update local_settings.py

--- mfp/mfp/local_settings.py
+++ mfp/mfp/local_settings.py
@@ -5,6 +5,6 @@ EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'originalugg2@gmail.com'
-EMAIL_HOST_PASSWORD = 'impossibleisnothing12'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
